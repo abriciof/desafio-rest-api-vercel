3606e76b2493ef15aa22d71d6e5af78ef7c18399
Fabricio Guimaraes <fabricio15.guimaraes@gmail.com>
2025-04-26 20:41:41 -0400

    feat: Adicionando supabase

--- core/settings.py
+++ core/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
@@ -113,10 +114,9 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
